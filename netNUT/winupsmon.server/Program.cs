af90b6bcd300094993a0446013fb8ce0d5556924
Sebastian Schocke <huntrckr@gmail.com>
2015-02-22 17:32:41 +0200

    Fixed namespace for winupsmon.server project

--- netNUT/winupsmon.server/Program.cs
+++ netNUT/winupsmon.server/Program.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace winupsmon.server
+namespace ScorpioTech.netNUT.winupsmon.server
 {
     class Program : ILogger
     {
